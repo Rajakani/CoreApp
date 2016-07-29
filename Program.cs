db2669517539ff053c296831e9f65db0cca8375a
Rajakani <rjkani@gmail.com>
2016-07-29 09:24:08 +0530

    Inital check in

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace CoreApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseStartup<Startup>()
+            .Build();
+            
+            host.Run();
+
+
+        }
+    }
+}
